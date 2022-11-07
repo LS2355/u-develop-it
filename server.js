c5c5b6a31b1e10867aaf195492317af0a859f1e3
Luis S <Luissalcedo2355@hotmail.com>
2022-11-07 14:44:54 -0800

    added voter routes

--- server.js
+++ server.js
@@ -1,145 +1,29 @@
-const express =  require('express');
+const express = require('express');
+const db = require('./db/connection');
+const apiRoutes = require('./routes/apiRoutes');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-const mysql = require('mysql2');
-const inputCheck =  require('./utils/inputCheck');
 
-//EXPRESS middleware
-app.use(express.urlencoded({extended: false}));
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Use apiRoutes
+app.use('/api', apiRoutes);
 
 
-//Connect to database
-const db = mysql.createConnection(
-    {
-    host: 'localhost',
-    //Your MySQL username,
-    user: 'root',
-    //Your MySQL password
-    password: 'M1M1ERDA2355_',
-    database: 'election'
-    },
-    console.log('Connected to the election database.')
-);
-
-
-//GET all candidates
-app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
-
-    db.query(sql, (err, rows) => {
-        if (err) { 
-            res.status(500).json({error: err.message});
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: rows
-        });
-    });
-}); 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-  });
-
-
-
-  //GET a single candidate
-  app.get('/api/candidate/:id',(req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
-    const params = [req.params.id];
-
-    db.query(sql, params, (err, row) => {
-        if (err) {
-            res.status(400).json({ error: err.message});
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: row
-        });
-    });
-  });
-
-
-
-
-  //Delete a candidate
-  app.delete('/api/candidate/:id', (req, res) => {
-    const sql = `DELETE FROM candidates WHERE id = ?`;
-    const params = [req.params.id];
-
-    db.query(sql, params, (err, result) => {
-        if (err) {
-            res.statusMessage(400).json({error: res,message});
-        }
-        else if (!result.affectedRows) {
-            res.json({
-                message: 'Candidate not found'
-            });
-        }
-        else {
-            res.json({
-                message: 'deleted',
-                changes: result.affectedRows,
-                id: req.params.id
-            });
-        };
-    });
-  });
-
-
-
-  //Create a candidate
-  app.post('/api/candidate', ({body},res) =>{
-    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
-    if (errors) {
-        res.status(400).json({ error: errors});
-        return;
-    }
-    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) VALUES (?,?,?)`;
-    const params = [body.first_name, body.last_name, body.industry_connected];
-
-    db.query(sql, params, (err, result) => {
-        if (err) {
-            res.status(400).json({error: err.message});
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: body
-        });
-    });
-
-  });
-
-
-
-  const sql =  `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)`;
-  const params = [1, 'Ronald', 'Firbank', 1];
-
-  db.query(sql, params, (err, result) => {
-    if (err)  {
-        console.log(err);
-    }
-    console.log(result);
-  });
-
-  app.get("/", (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
-});
-
-
-
-//Default request for any other request (NOT FOUND)
+// Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 
-app.listen(PORT, () => {
-    console.log(`Server running on PORT ${PORT}`);
+// Start server after DB connection
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database connected.');
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
